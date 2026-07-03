490d55ac644d465db010f5c0caf60adf2ee823e0
Delta Student <krishnagupta2402@gmail.com>
2026-07-03 16:45:44 +0530

    commit

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 import flappy_bird_gymnasium
 import gymnasium as gym
 import torch
+from dqn import DQN
 
 if torch.backends.mps.is_available():
     device = "mps"
@@ -9,20 +10,26 @@ elif torch.cuda.is_available():
 else:
     device = "cpu"
 
-env = gym.make("FlappyBird-v0", render_mode="human", use_lidar=True)
+def run(self, is_training=True, render = False):
+    env = gym.make("FlappyBird-v0", render_mode="human" if render else None)# this becomes true for testing and false for training
 
-state, _ = env.reset()
+    num_states = env.observation_space.shape[0] #input dim
+    num_actions = env.action_space.n #output dimension (output_dim)
 
-while True:
-    # Next action:
-    # (feed the observation to your agent here)
-    action = env.action_space.sample()
+    policy_dqn = DQN(num_states, num_actions).to(device)
 
-    # Processing: terminated => done
-    next_state, reward, terminated, _, _ = env.step(action)
+    state, _ = env.reset()
+
+    while True:
+      # Next action:
+      # (feed the observation to your agent here)
+      action = env.action_space.sample()
+
+      # Processing: terminated => done
+      next_state, reward, terminated, _, _ = env.step(action)
     
-    # Checking if the player is still alive
-    if terminated:
-        break
+      # Checking if the player is still alive
+      if terminated:
+          break
 
-env.close()
+    env.close()
