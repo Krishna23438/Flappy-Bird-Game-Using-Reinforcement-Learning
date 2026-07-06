e73348d7a3a79b71fb7e541e115d8d993c3b3227
Delta Student <krishnagupta2402@gmail.com>
2026-07-06 19:55:32 +0530

    Add all Files

--- agent.py
+++ agent.py
@@ -164,10 +164,10 @@ class Agent:
             self.optimizer.step()
 
 if __name__ == "__main__":
-    #Parse command line inputs
+    # Parse command line inputs
     parser = argparse.ArgumentParser(description='Train or test model.')
-    parser.add_argument('hyperparameters',help='')
-    parser.add_argument('--train',help='Training mode', action='store_true')
+    parser.add_argument('hyperparameters', help='')
+    parser.add_argument('--train', help='Training mode', action='store_true')
     args = parser.parse_args()
 
     dql = Agent(param_set=args.hyperparameters)
